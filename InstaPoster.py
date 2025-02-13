81b3479c7bb9e2aa71484e438fdad87c5de68d2e
Leonardo Caricola <L_Caricola@esterni.vittoriaassicurazioni.it>
2025-02-13 15:49:28 +0100

    fix per commit

--- InstaPoster.py
+++ InstaPoster.py
@@ -25,9 +25,7 @@ class InstagramBot:
         # Crea un oggetto ChromeOptions per impostazioni specifiche
         options = webdriver.ChromeOptions()
         ##aggiunta per configurazione yaml
-        options.add_argument(f'--user-data-dir={user_data_dir}')
         options.add_argument('--disable-notifications')  # Disabilita le notifiche del browser
-        #options.add_argument('--headless')  # Esegui in modalità headless
         options.add_argument('--no-sandbox')  # Necessario per alcuni ambienti di container
         options.add_argument('--disable-dev-shm-usage')  # Per evitare errori di memoria in container
 
